f8159f32086ed135087269afb8551eb6b033379d
Melquíades <melquiades.rodrigues@hotmail.com>
2021-05-17 19:27:57 -0300

    fix: Adequar containers docker

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -9,15 +9,6 @@ php artisan config:cache
 
 >&2 echo "Updated dependencies"
 >&2 echo "=================================================================="
->&2 echo "Waiting for mysql to be ready"
-
-until mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USERNAME" -p"$MYSQL_PASSWORD" --database="$MYSQL_DATABASE" --execute="SELECT count(table_name) > 0 FROM information_schema.tables;" --skip-column-names -B; do
-    sleep 10
-done
-
->&2 echo "Connected to mysql"
->&2 echo "=================================================================="
->&2 echo "Executing migrations"
 
 php artisan migrate
 
