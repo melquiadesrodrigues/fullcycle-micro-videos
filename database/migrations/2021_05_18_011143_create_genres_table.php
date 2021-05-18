9b76b8e519238aa9c09b8b1c9d00f68dbc8216c9
Melquíades <melquiades.rodrigues@hotmail.com>
2021-05-17 23:22:47 -0300

    feat: Adicionar recursos de gêneros

--- database/migrations/2021_05_18_011143_create_genres_table.php
+++ database/migrations/2021_05_18_011143_create_genres_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateGenresTable extends Migration
 {
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
             $table->timestamps();
             $table->softDeletes();
@@ -20,6 +19,6 @@ class CreateCategoriesTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('genres');
     }
 }
